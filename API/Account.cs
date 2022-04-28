e0be4c295d8b1b40e1e8a9be3c8ef896b0443d75
lufeiyuan83 <232311109@qq.com>
2022-04-28 10:19:55 +0800

    添加项目文件。

---
+++ API/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace API
+{
+    public class Account
+    {
+        public string Username { get; set; } = "lfy_83";
+        public void PrintName(string name)
+        {
+            Console.WriteLine("Hello World! " + name);
+        }
+    }
+}
---
+++ Reflect/Program.cs
@@ -0,0 +1,36 @@
+﻿using API;
+using System;
+using System.Reflection;
+
+namespace Reflect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.Load("API");
+            Type type = assembly.GetType("API.Account",true,false);
+            Account account = (Account)Activator.CreateInstance(type);
+            //Account account = (Account)assembly.CreateInstance("API.Account");
+
+            #region 属性反射
+            PropertyInfo property = account.GetType().GetProperty("Username");
+            if (property != null)
+            {
+                Console.WriteLine(property.GetValue(account, null));
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+            #endregion
+
+            #region 方法反射
+            MethodInfo method = type.GetMethod("PrintName");
+            object kiba = assembly.CreateInstance("API.Account");
+            object[] pmts = new object[] { "lufeiyuan" };
+            method.Invoke(kiba, pmts);//执行方法 
+            #endregion
+        }
+    }
+}
